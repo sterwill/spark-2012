0af59c9736b1839bc611c5288c0a41b6fa43249e
Shaw Terwilliger <sterwill@tinfig.com>
2012-08-11 15:18:24 -0400

    DDL is auto generating and we can save models!

--- tailfeather/src/main/java/org/tailfeather/controller/UserController.java
+++ tailfeather/src/main/java/org/tailfeather/controller/UserController.java
@@ -5,17 +5,27 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+import org.tailfeather.dao.UserDao;
+import org.tailfeather.service.UserService;
 
 public class UserController implements Controller {
+	@Autowired
+	private UserService userService;
 
 	protected final Log logger = LogFactory.getLog(getClass());
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		logger.info("returning a tailfeather view");
+
+		UserDao user = new UserDao();
+		user.setEmail("sterwill@tinfig.com");
+		user.setFullName("Shaw Terwilliger");
+		userService.create(user);
+
 		return new ModelAndView("user.jsp");
 	}
 }
