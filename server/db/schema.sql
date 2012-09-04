4374e03656f764fcb121cc3d61b7228be024ba2e
Shaw Terwilliger <sterwill@tinfig.com>
2012-09-03 22:11:10 -0400

    Explicit schema

---
+++ server/db/schema.sql
@@ -0,0 +1,21 @@
+drop table if exists checkins cascade;
+drop table if exists locations cascade;
+drop table if exists users cascade;
+
+create table users (
+  id text primary key,
+  email text unique not null,
+  full_name text not null
+);
+
+create table locations (
+  id text primary key,
+  name text not null
+);
+
+create table checkins (
+  id text primary key,
+  time timestamp without time zone not null,
+  location_id text not null references locations(id) on delete cascade on update cascade,
+  user_id text not null references users(id) on delete cascade on update cascade
+);
