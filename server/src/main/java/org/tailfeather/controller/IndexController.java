181ae761d608ad5f9b98cb7ee53defc0d8af4e70
Shaw Terwilliger <sterwill@tinfig.com>
2012-09-04 20:09:23 -0400

    handle favicon.ico

--- server/src/main/java/org/tailfeather/controller/IndexController.java
+++ server/src/main/java/org/tailfeather/controller/IndexController.java
@@ -1,5 +1,8 @@
 package org.tailfeather.controller;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Response.Status;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,8 +11,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class IndexController {
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
 		return "index.jsp";
+	}
+
+	@RequestMapping(value = "/favicon.ico", method = RequestMethod.GET)
+	public void favicon(HttpServletResponse response) {
+		response.setStatus(Status.NOT_FOUND.getStatusCode());
 	}
 }
